ede56c35638b522cc749c28393c1863864dee214
Ashwin Padaki <apadaki22@gmail.com>
2024-12-14 09:54:45 -0500

    update tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,13 @@ export default {
 			'title': ['Noto Sans','Roboto','Helvetica','Arial','sans-serif'],
 		},
 		extend: {},
+		fontSize: {
+			sm: ['0.87rem', '18pt'],
+			base: ['0.97rem', '18pt'],
+			xl: ['1.333rem', '24pt'],
+			'2xl': ['1.667rem', '24pt'],
+			'3xl': '2rem'
+		}
 	},
 	plugins: [],
 }
