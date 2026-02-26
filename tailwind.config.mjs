58872f0fd624bb0ec59a0cf61e03e4277bf4a835
Ashwin Padaki <apadaki22@gmail.com>
2026-02-26 09:50:29 -0500

    updated papers

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,33 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
 	theme: {
 		fontFamily: {
 			'main': ['Geneva', 'Open Sans', 'sans-serif'],
 			'title': ['Geneva','Open Sans', 'sans-serif'],
 			'mono': ['JetBrains Mono', 'monospace'],
-		},
-		extend: {
-			colors: {
-				primary: {
-					50: '#f0f9ff',
-					100: '#e0f2fe',
-					200: '#bae6fd',
-					300: '#7dd3fc',
-					400: '#38bdf8',
-					500: '#0ea5e9',
-					600: '#0284c7',
-					700: '#0369a1',
-					800: '#075985',
-					900: '#0c4a6e',
-				},
-				dark: {
-					100: '#1E293B',
-					200: '#0F172A',
-					300: '#020617',
-				}
-			},
 		},
 	},
 	plugins: [
