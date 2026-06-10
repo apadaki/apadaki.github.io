b6df4123607387d4ba800302a2ffce6550e894cd
Ashwin Padaki <ashwin@pinecone.io>
2026-06-09 23:17:36 -0400

    remove blog

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,1 @@
-import { defineCollection, z } from 'astro:content';
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
-
-const blogCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    date: z.string(),
-    excerpt: z.string(),
-  }),
-});
-
-export const collections = {
-  'blog': blogCollection,
-}; 
+export const collections = {};
